17d3524206600babb335412b59acef9ed8956a99
Edo <hello@edoardo-hb.it>
2024-01-23 16:02:09 +0100

    feat: added initial logic

--- result/index.php
+++ result/index.php
@@ -6,6 +6,21 @@
   <title>Document</title>
 </head>
 <body>
-  
+  <h1>Risultato</h1>
+
+  <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+      $paragraph = $_GET['paragraph'];
+      $badword = $_GET['badword'];
+
+      echo "<p>Paragrafo Originale: $paragraph</p>";
+
+      $censored = str_ireplace($badword, '***', $paragraph);
+
+      echo "<p>Paragrafo censurato: $censored</p>";
+    } else {
+      echo "<h1>Request Method not allowed</h1>";
+    }
+  ?>
 </body>
 </html>
