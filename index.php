ec1338527fed363a094240c0e7634e59de85ee22
Edo <hello@edoardo-hb.it>
2024-01-23 15:58:38 +0100

    feat: added form

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
   <title>Document</title>
 </head>
 <body>
+  <h1>PHP Badwords</h1>
+
+  <form action="result" method="GET">
+    <label for="paragraph">Inserisci il Paragrafo</label> <br />
+    <textarea name="paragraph" id="paragraph" cols="50" rows="4" required></textarea> <br /><br />
+
+    <label for="badword">Parola da censurare</label> <br />
+    <input type="text" name="badword" id="badword" required> <br />
+    
+    <input type="submit" value="Invia">
+  </form>
   
 </body>
 </html>
